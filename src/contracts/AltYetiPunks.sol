831856407207772230a2af5f8f33eba8a0724bcc
Chris Foley <chrisninetyone@gmail.com>
2021-12-22 21:45:22 -0800

    Removed some fat

--- src/contracts/AltYetiPunks.sol
+++ src/contracts/AltYetiPunks.sol
@@ -5,9 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// Be careful if minting from the contract. Make sure you understand how 
-// the pricing works before smashing your keyboard in frustration.
-contract WutIsThis is ERC721, Ownable {
+contract YetiPunks is ERC721, Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenSupply;
@@ -15,58 +13,44 @@ contract WutIsThis is ERC721, Ownable {
     uint256 public constant MAX_SUPPLY = 5000;
     
     uint256 public cost = 0.03 ether;
-    // uint256 public constant PRICE_WAVE_ONE =     0.5 ether;  // 100 Available
-    // uint256 public constant PRICE_WAVE_TWO =     0.6 ether;  // 150 Available
-    // uint256 public constant PRICE_WAVE_THREE =   0.7 ether;  // 250 Available
 
     bool public saleIsActive = false;
     string private _baseTokenURI;
 
-    // Constructor, because why not? (I don't know why)
-    constructor() ERC721("YetiPunks", "YETIPUNKS") public { }
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        // string memory _initBaseURI,
+        // string memory _initNotRevealedUri,
+        // uint256 _revealTime
+    ) ERC721(_name, _symbol) public { 
+        //Set baseURI here?
+        //set pre-reveal image here?
+        //set deploy time here?
+    }
 
     // This is the new mint function leveraging the counter library
     function mint() public payable {
-        require(saleIsActive, "JPEGs are not for sale yet!");
+        require(saleIsActive, "Yetis are not for sale yet!");
 
         uint256 mintIndex = _tokenSupply.current() + 1; // Start IDs at 1
-        require(mintIndex <= MAX_SUPPLY, "JPEGs are sold out!");
+        require(mintIndex <= MAX_SUPPLY, "Yetis are sold out!");
 
         uint256 mintPrice = currentPrice();
-        require(msg.value >= mintPrice, "Not enough ETH to buy a JPEG!");
+        require(msg.value >= mintPrice, "Not enough ETH to buy a Yeti!");
 
-        // Mint. That's the easy part.
         _tokenSupply.increment();
         _safeMint(msg.sender, mintIndex);
     }
 
-    // How much is this JPEG going to cost me?
-    function currentPrice() public view returns (uint256) {
-
-        uint256 totalMinted = _tokenSupply.current();
-
-        if (totalMinted < 100) {
-            return PRICE_WAVE_ONE;
-
-        } else if (totalMinted < 250) {
-            return PRICE_WAVE_TWO;
-
-        } else {
-            return PRICE_WAVE_THREE;
-        }
-    }
-
-    // I wonder how many JPEGs are left?
     function remainingSupply() public view returns (uint256) {
         return MAX_SUPPLY - _tokenSupply.current();
     }
 
-    // I wonder how many JPEGs are minted?
     function tokenSupply() public view returns (uint256) {
         return _tokenSupply.current();
     }
 
-    // All the functions you don't really care about but need to be here
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
@@ -75,14 +59,12 @@ contract WutIsThis is ERC721, Ownable {
         _baseTokenURI = baseURI;
     }
 
-    // Go go go!
     function toggleSale() public onlyOwner {
         saleIsActive = !saleIsActive;
     }
 
-    // You had to expect this function, right?
     function withdrawBalance() public onlyOwner {
-        //divide evenly between all 3 wallets
+        //TODO: divide evenly between 3 wallets
         payable(msg.sender).transfer(address(this).balance);
     }
 }
