74b17d035ef079d236dc03f35f3ee203df4c7122
Chris Foley <chrisninetyone@gmail.com>
2021-12-29 18:39:51 -0800

    Made text all caps, changed spacing of avatars

--- src/components/About.js
+++ src/components/About.js
@@ -3,39 +3,33 @@ import Tokimori00 from '../images/yetipunkv4white.png'
 
 const About = () => {
     return (
-        <div className = "row font">
-            <div className = 'float-text'>
-                <h1>About Yeti Punk Labs</h1>
+        <div className="font d-flex flex-row justify-content-around">
+            <div className='d-flex flex-column align-items-center'>
+                <div className='avatar'>
+                    <img className='avatar' src={Tokimori00} />
+                </div>
+                <div className='d-flex flex-column align-items-center'>
+                    <h6>Tokimori00</h6>
+                    <div>Artist, NFT Chef</div>
+                </div>
             </div>
-            
-            <div className = 'col-sm'>
-            <div className='avatar float-left'>
-                <img className = 'avatar' src={Tokimori00}/>
-            </div>
-            <div className = 'col-sm float-right'>
-                <h6>Tokimori00</h6>
-                <div>LFGGGGGGG</div>
-            </div>
-            
-            </div>
-            <div className = 'col-sm'>
-                <div className='avatar float-left'>
-                    <img className = 'avatar' src={Tokimori00}/>
+            <div className='d-flex flex-column align-items-center'>
+                <div className='avatar'>
+                    <img className='avatar' src={Tokimori00} />
                 </div>
                 <div>
-                    <h6>PakMinSoo</h6>
-                    <div>Did you call me?</div>
+                    <h6>Tenzing</h6>
+                    <div>Sherpa</div>
                 </div>
             </div>
-            <div className = 'col-sm'>
-            <div className='avatar float-left'>
-                <img className = 'avatar' src={Tokimori00}/>
-            </div>
-            <div>
-                <h6>WangMandu</h6>
-                <div>I hate Javascript</div>
-            </div>
-
+            <div className='d-flex flex-column align-items-center'>
+                <div className='avatar'>
+                    <img className='avatar' src={Tokimori00} />
+                </div>
+                <div>
+                    <h6>WangMandu</h6>
+                    <div>Dev</div>
+                </div>
             </div>
         </div>
     )
