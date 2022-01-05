5207607b7158010b23c713188a27b4b459f85f54
willschultz2@gmail.com <willschultz2@gmail.com>
2022-01-04 21:55:22 -0800

    added event and new require statement

--- src/contracts/YetiPunks.sol
+++ src/contracts/YetiPunks.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract YetiPunks is ERC721, Ownable {
     
     string public baseURI;
-    
+    event yetiMintedAmount(uint16 amount);
     uint16 MAX_YETIS = 10000;
     uint16 totalSupply = 0;
 
@@ -18,6 +18,7 @@ contract YetiPunks is ERC721, Ownable {
     function withdraw() public onlyOwner {
         uint256 balance = address(this).balance;
         payable(msg.sender).transfer(balance);
+
     }
 
     function setBaseURI(string memory base) public onlyOwner {
@@ -29,13 +30,16 @@ contract YetiPunks is ERC721, Ownable {
     }
 
     // Despite the function being payable, I've removed any functionality for that here for the simplicity of the example.
-    function mint(uint256 numberOfTokens) public payable {
+    function mint(uint16 numberOfTokens) public payable {
         require(totalSupply + 1 < MAX_YETIS, "All yeti punks have already been minted!");
+        require(totalSupply + numberOfTokens <= MAX_YETIS, "You are trying to mint more Yetis thatn are left, decrease the number of yetis and try again.");
         
         for (uint i = 0; i < numberOfTokens; i++) {
             _safeMint(msg.sender, totalSupply + 1);
             totalSupply += 1;
         }
+
+        emit yetiMintedAmount(numberOfTokens);
     }
 
     function tokenURI(uint256 tokenId) public view virtual override(ERC721) returns (string memory) {
