2f684448cc5e0a71f2e8a9952f3ff30664de48cc
Chris Foley <chrisninetyone@gmail.com>
2022-01-06 21:48:44 -0800

    Refactor about to make it cleaner

--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,40 @@
 import React from 'react'
 import yetitar from '../images/yetipunk_bios.png'
 
+const yetis = [
+    {
+        name: 'Tokimori',
+        profile: yetitar,
+        bio: 'The artist who made this all happen. His bond with yeti punks is cemented through a love for kush.'
+    },
+    {
+        name: 'Tenzing',
+        profile: yetitar,
+        bio: 'A developer who brought together Tokimori and WangMandoo to create a trio of yeti heads.'
+    },
+    {
+        name: 'WangMandu',
+        profile: yetitar,
+        bio: 'A king-sized dumpling skilled in dev and nft art'
+    }
+]
+
 const About = () => {
     return (
-        <div className="font bio-section">
-            <div className='bio-square'>
-                <div className='d-flex flex-column align-items-center'>
-                    <img className='avatar' src={yetitar} alt="yeti-avatar" />
-                    <h6 className='mt-3'>Tokimori</h6>
-                </div>
-                <div className='bio'>
-                    <p>The artist who made this all happen. His bond with yeti punks is cemented through a love for kush.</p>
-                </div>
-            </div>
-            <div className='bio-square'>
-                <div className='avatar d-flex flex-column align-items-center'>
-                    <img className='avatar' src={yetitar} alt="yeti-avatar" />
-                    <h6 className='mt-3'>Tenzing</h6>
-                </div>
-                <div className='bio'>
-                    <div>A developer who brought together Tokimori and WangMandoo to create a trio of yeti heads.</div>
-                </div>
-            </div>
-            <div className='bio-square'>
-                <div className='avatar d-flex flex-column align-items-center'>
-                    <img className='avatar' src={yetitar}  alt="yeti-avatar" />
-                    <h6 className='mt-3'>WangMandu</h6>
-                </div>
-                <div className='bio'>
-                    <div>A king-sized dumpling skilled in dev and nft art</div>
-                </div>
-            </div>
+        <div className="bio-section">
+            {
+                yetis.map ( yeti =>
+                    <div className='bio-square'>
+                        <div className='bio-card'>
+                            <img className='avatar' src={yeti.profile} alt="yeti-avatar" />
+                            <h6>{yeti.name}</h6>
+                        </div>
+                        <div className='bio'>
+                            <p>{yeti.bio}</p>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
