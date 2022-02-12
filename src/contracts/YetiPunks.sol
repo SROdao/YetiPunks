a47ddc5f868e95ab2d3bed833f15c2deaf2cdcb5
Chris Foley <chrisninetyone@gmail.com>
2022-02-12 10:58:11 -0500

    modified allowlistMint, added notRevealedUri, formatting

--- src/contracts/YetiPunks.sol
+++ src/contracts/YetiPunks.sol
@@ -8,145 +8,160 @@ import "./ERC721A.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract YetiPunks is Ownable, ERC721A, ReentrancyGuard {
-  uint256 public immutable maxPerAddressDuringMint;
-  uint256 public immutable amountForDevs;
-  uint256 public immutable amountForAuctionAndDev;
+    uint256 public immutable maxPerAddressDuringPublicSale;
+    uint256 public immutable maxPerAddressDuringPresale = 5;
+    uint256 public immutable amountForDevs;
+    uint256 public immutable amountForAuctionAndDev;
+    string public notRevealedUri;
 
-  mapping(address => uint256) public allowlist;
+    mapping(address => uint256) public allowlist; //seed this with the uint being 5
 
-  constructor(
-    uint256 maxBatchSize_,
-    uint256 collectionSize_,
-    uint256 amountForAuctionAndDev_,
-    uint256 amountForDevs_
-  ) ERC721A("Petty Monks", "PM", maxBatchSize_, collectionSize_) {
-    maxPerAddressDuringMint = maxBatchSize_;
-    amountForAuctionAndDev = amountForAuctionAndDev_;
-    amountForDevs = amountForDevs_;
-    require(
-      amountForAuctionAndDev_ <= collectionSize_,
-      "larger collection size needed"
-    );
-  }
-
-  modifier callerIsUser() {
-    require(tx.origin == msg.sender, "The caller is another contract");
-    _;
-  }
-
-  function allowlistMint() external payable callerIsUser {
-    uint256 price = 0.03 ether;
-    require(allowlist[msg.sender] > 0, "not eligible for allowlist mint");
-    require(totalSupply() + 1 <= collectionSize, "reached max supply");
-    allowlist[msg.sender]--;
-    _safeMint(msg.sender, 1);
-    refundIfOver(price);
-  }
-
-  function publicSaleMint(uint256 quantity)
-    external
-    payable
-    callerIsUser
-  {
-
-    uint256 publicPrice = 0.03 ether;
-
-    require(totalSupply() + quantity <= collectionSize, "reached max supply");
-    require(
-      numberMinted(msg.sender) + quantity <= maxPerAddressDuringMint,
-      "can not mint this many"
-    );
-    _safeMint(msg.sender, quantity);
-    refundIfOver(publicPrice * quantity);
-  }
-
-  function refundIfOver(uint256 price) private {
-    require(msg.value >= price, "Need to send more ETH.");
-    if (msg.value > price) {
-      payable(msg.sender).transfer(msg.value - price);
+    constructor(
+        uint256 maxBatchSize_,
+        uint256 collectionSize_,
+        uint256 amountForAuctionAndDev_,
+        uint256 amountForDevs_,
+        string memory _initNotRevealedUri
+    ) ERC721A("Petty Monks", "PM", maxBatchSize_, collectionSize_) {
+        maxPerAddressDuringPublicSale = maxBatchSize_;
+        amountForAuctionAndDev = amountForAuctionAndDev_;
+        amountForDevs = amountForDevs_;
+        require(
+            amountForAuctionAndDev_ <= collectionSize_,
+            "larger collection size needed"
+        );
+        setNotRevealedURI(_initNotRevealedUri);
     }
-  }
 
-  function isPublicSaleOn(
-    uint256 publicPriceWei,
-    uint256 publicSaleKey,
-    uint256 publicSaleStartTime
-  ) public view returns (bool) {
-    return
-      publicPriceWei != 0 &&
-      publicSaleKey != 0 &&
-      block.timestamp >= publicSaleStartTime;
-  }
-
-  function seedAllowlist(address[] memory addresses, uint256[] memory numSlots)
-    external
-    onlyOwner
-  {
-    require(
-      addresses.length == numSlots.length,
-      "addresses does not match numSlots length"
-    );
-    for (uint256 i = 0; i < addresses.length; i++) {
-      allowlist[addresses[i]] = numSlots[i];
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, "The caller is another contract");
+        _;
     }
-  }
 
-  // For marketing etc.
-  function devMint(uint256 quantity) external onlyOwner {
-    require(
-      totalSupply() + quantity <= amountForDevs,
-      "too many already minted before dev mint"
-    );
-    require(
-      quantity % maxBatchSize == 0,
-      "can only mint a multiple of the maxBatchSize"
-    );
-    uint256 numChunks = quantity / maxBatchSize;
-    for (uint256 i = 0; i < numChunks; i++) {
-      _safeMint(msg.sender, maxBatchSize);
+    function allowlistMint(uint256 quantity) external payable callerIsUser {
+        uint256 price = 0.03 ether;
+        require(allowlist[msg.sender] > 0, "not eligible for whitelist mint");
+        require(totalSupply() + 1 <= collectionSize, "exceeded max supply"); //is this possible?
+        require(
+            numberMinted(msg.sender) + quantity <= maxPerAddressDuringPresale,
+            "mint amount exceeds whitelist"
+        );
+        allowlist[msg.sender] -= quantity;
+        _safeMint(msg.sender, quantity);
+        refundIfOver(price * quantity);
     }
-  }
 
-  // metadata URI
-  string private _baseTokenURI;
+    function publicSaleMint(uint256 quantity) external payable callerIsUser {
+        uint256 publicPrice = 0.03 ether;
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return _baseTokenURI;
-  }
+        require(
+            totalSupply() + quantity <= collectionSize,
+            "exceeded max supply"
+        );
+        require(
+            numberMinted(msg.sender) + quantity <=
+                maxPerAddressDuringPublicSale,
+            "can not mint this many"
+        );
+        _safeMint(msg.sender, quantity);
+        refundIfOver(publicPrice * quantity);
+    }
 
-  function setBaseURI(string calldata baseURI) external onlyOwner {
-    _baseTokenURI = baseURI;
-  }
+    function refundIfOver(uint256 price) private {
+        require(msg.value >= price, "Need to send more ETH.");
+        if (msg.value > price) {
+            payable(msg.sender).transfer(msg.value - price);
+        }
+    }
 
-  function withdrawBalance() public onlyOwner {
-      uint256 oneThird = (address(this).balance * 33) / 100;
-      (bool manduSuccess, ) = payable(
-          0xD61ADc48afE9402B4411805Ce6026eF74F94E713
-      ).call{value: oneThird}("");
-      require(manduSuccess);
-      (bool tenzingSuccess, ) = payable(
-          0xE3Ce04B3BcbdFa219407870Ca617e18fBF503F28
-      ).call{value: oneThird}("");
-      require(tenzingSuccess);
-      (bool tokiMoriSuccess, ) = payable(
-          0x49Cf0aF1cE6a50e822A91a427B3E29007f9C6C09
-      ).call{value: address(this).balance}("");
-      require(tokiMoriSuccess);
-  }
+    function isPublicSaleOn(
+        uint256 publicPriceWei,
+        uint256 publicSaleKey,
+        uint256 publicSaleStartTime
+    ) public view returns (bool) {
+        return
+            publicPriceWei != 0 &&
+            publicSaleKey != 0 &&
+            block.timestamp >= publicSaleStartTime;
+    }
 
-  function setOwnersExplicit(uint256 quantity) external onlyOwner nonReentrant {
-    _setOwnersExplicit(quantity);
-  }
+    function seedAllowlist(
+        address[] memory addresses,
+        uint256[] memory numSlots
+    ) external onlyOwner {
+        require(
+            addresses.length == numSlots.length,
+            "addresses does not match numSlots length"
+        );
+        for (uint256 i = 0; i < addresses.length; i++) {
+            allowlist[addresses[i]] = numSlots[i];
+        }
+    }
 
-  function numberMinted(address owner) public view returns (uint256) {
-    return _numberMinted(owner);
-  }
+    // For marketing etc.
+    function devMint(uint256 quantity) external onlyOwner {
+        require(
+            totalSupply() + quantity <= amountForDevs,
+            "too many already minted before dev mint"
+        );
+        require(
+            quantity % maxBatchSize == 0,
+            "can only mint a multiple of the maxBatchSize"
+        );
+        uint256 numChunks = quantity / maxBatchSize;
+        for (uint256 i = 0; i < numChunks; i++) {
+            _safeMint(msg.sender, maxBatchSize);
+        }
+    }
 
-  function getOwnershipData(uint256 tokenId)
-    external
-    view
-    returns (TokenOwnership memory)
-  {
-    return ownershipOf(tokenId);
-  }
+    // metadata URI
+    string private _baseTokenURI;
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+        _baseTokenURI = _notRevealedURI;
+    }
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function withdrawBalance() public onlyOwner {
+        uint256 oneThird = (address(this).balance * 33) / 100;
+        (bool manduSuccess, ) = payable(
+            0xD61ADc48afE9402B4411805Ce6026eF74F94E713
+        ).call{value: oneThird}("");
+        require(manduSuccess);
+        (bool tenzingSuccess, ) = payable(
+            0xE3Ce04B3BcbdFa219407870Ca617e18fBF503F28
+        ).call{value: oneThird}("");
+        require(tenzingSuccess);
+        (bool tokiMoriSuccess, ) = payable(
+            0x49Cf0aF1cE6a50e822A91a427B3E29007f9C6C09
+        ).call{value: address(this).balance}("");
+        require(tokiMoriSuccess);
+    }
+
+    function setOwnersExplicit(uint256 quantity)
+        external
+        onlyOwner
+        nonReentrant
+    {
+        _setOwnersExplicit(quantity);
+    }
+
+    function numberMinted(address owner) public view returns (uint256) {
+        return _numberMinted(owner);
+    }
+
+    function getOwnershipData(uint256 tokenId)
+        external
+        view
+        returns (TokenOwnership memory)
+    {
+        return ownershipOf(tokenId);
+    }
 }
