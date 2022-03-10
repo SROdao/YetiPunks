0a43727d7d34481e496b3eb08f92ace505eb3c2a
willschultz2@gmail.com <willschultz2@gmail.com>
2022-03-09 18:29:11 -0800

    lowercased redwood

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,7 @@ const Main = ({
                 </div>
                 <div className="block-text">
                     <p>
-                    Deep in the Valhalla Mountains live the YETIPUNKS. Far away from civilization and far away from the chaos. They live a simple life, filled with good vibes and only the best herb. The streams from the snow capped mountains are filled with magical nutrients that help grow nugs the size of a Redwood tree. 
+                    Deep in the Valhalla Mountains live the YETIPUNKS. Far away from civilization and far away from the chaos. They live a simple life, filled with good vibes and only the best herb. The streams from the snow capped mountains are filled with magical nutrients that help grow nugs the size of a redwood tree. 
                     </p><p>
                     All YETIPUNKS have different traits and features but they pride themselves on inclusivity and community. They're always down to lend a helping hand, so never fear… you will always have a fren in the YETIPUNKS.
                     </p>
